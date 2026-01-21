fbda2199034401432a629cf58f936d046b561ac9
ajitali <ajitabi123@gmail.com>
2026-01-21 14:04:42 +0530

    karth paranju

---
+++ karthika.java
@@ -0,0 +1,20 @@
+// import java.util.*;
+class Abhay {
+    int id = 10;
+    String name = "Abhay kaaarr";
+
+    void display() {
+        System.out.println(id + " " + name);
+    }
+}
+// 
+
+public class karthika extends Abhay{
+    public static void main(String args[]){
+         System.out.println();
+       
+    }
+}
+
+
+//define a class product with data members pcode, pname and price. Create a 3 objects of class and find the product having the lowest price.
