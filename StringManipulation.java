728450af12e43b26b1b69cbc02ca6a0eb8b8581a
ajitali <ajitabi123@gmail.com>
2026-02-16 15:49:29 +0530

    Just some print statements addition

--- StringManipulation.java
+++ StringManipulation.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("String Manipulation in JAVA");
         System.out.print("Enter a string: ");
         String str = sc.nextLine();
 
