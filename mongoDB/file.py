7e7f5fda4a3d0b41d0a35e0ac1efbeeb9c0500bc
ajitali <ajitabi123@gmail.com>
2026-03-26 10:51:22 +0530

    mongo db file1

---
+++ mongoDB/file.py
@@ -0,0 +1,13 @@
+import pymongo
+conn = pymongo.MongoClient("mongodb://localhost:27017/")
+# print(conn.list_database_names())
+db = conn["test"]
+# print(db.list_collection_names())
+col = db["Marks"]
+data = {"Roll_No": 2,"Name":"Aarjav", "Age": 22}
+x = col.insert_one(data)
+if(x):
+    print("inserted")
+else:
+    print("Error")
+    
