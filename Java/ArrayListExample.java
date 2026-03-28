b0b0725121f78eddbf50184b0adf09179fd47d6f
ajitali <ajitabi123@gmail.com>
2026-03-28 12:54:44 +0530

    Array List

---
+++ Java/ArrayListExample.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Mango");
+
+        System.out.println("List: " + list);
+
+        System.out.println("First element: " + list.get(0));
+
+        list.remove("Banana");
+
+        System.out.println("After removal: " + list);
+    }
+}
